cfbde8ba2bcb32636fb9350aab916d503d6a72da
0xCC <31476295+Funya1337@users.noreply.github.com>
2024-06-01 00:58:30 +0300

    first commit

---
+++ shadcoinbot/index.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const TelegramBot = require('node-telegram-bot-api');
+
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+
+const bot = new TelegramBot(process.env.TG_TOKEN, {polling: true});
+const webAppUrl = 'https://ya.ru';
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+  
+    if (text === '/start') {
+      await bot.sendMessage(chatId, 'Make an order!', {
+          reply_markup: {
+              inline_keyboard: [
+                  [{text: "Make an order", web_app: {url: webAppUrl}}]
+              ]
+          }
+        });
+    }
+  });
+
+// bot.on('message', (msg) => {
+//   const chatId = msg.chat.id;
+//   bot.sendMessage(chatId, 'Received your message');
+// });
